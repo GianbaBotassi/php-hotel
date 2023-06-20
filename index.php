e9c2755d3cc56bc39b263b42efae6bfad3ca5776
GianbaBotassi <gianba89@gmail.com>
2023-06-20 16:34:35 +0200

    Add Bootstrap cdn

---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php
+  
+  $hotels = [
+  
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+  
+  ];
+  
+  // echo var_dump($hotels);
+  
+  
+  echo "<table class='table'><thead><tr>";
+  foreach ($hotels[0] as $key => $value) {
+    echo "<th scope='col'>" . $key . "</th>";
+  };
+  echo "</tr></thead>";
+
+  echo "<tbody>";
+
+  foreach ($hotels as $hotel) {
+    echo "<tr>"
+    foreach ($hotel as $value) {
+      var_dump($value); 
+      echo "<td>" . $value . "</td>";
+    };
+    echo "</tr>"
+  };
+
+  
+  echo "</tr></tbody></table>";
+
+  echo "<br /> <br />";
+
+
+
+
+
+
+
+
+  
+  ?>
+  <table class="table">
+
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+  
+
+  </body>
+</html>
