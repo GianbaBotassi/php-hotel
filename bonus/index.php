4597f325ebfe9cf9802d64cab65429353fd93859
GianbaBotassi <gianba89@gmail.com>
2023-06-20 18:21:47 +0200

    Add bonus checkbox filter with submit button

--- bonus/index.php
+++ bonus/index.php
@@ -6,10 +6,21 @@
     <title>PHP Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+    <style>
+      input[type="submit"]{
+        display: block;
+        margin-top:16px;
+      }
+    </style>
+
   </head>
   <body>
 
+  <form class="container">
+    <input type="checkbox" name="parking">
+    <label for="parking">Parking</label>
 
+    <input type="submit" value="Submit">
     <?php
   
   $hotels = [
@@ -68,6 +79,7 @@
 
   foreach ($hotels as $hotel) {
 
+      if($_GET == [] || $_GET["parking"] == ""){
       echo "<tr>";
   
       foreach ($hotel as $key => $value) {
@@ -75,12 +87,26 @@
       };
   
       echo "</tr>";
-    }
+    } else {    //condizione se checkbox parking è true come la key dell'hotel
+      $parkingOn = isset($_GET['parking']);
+      if($hotel["parking"] == $parkingOn){
+        echo "<tr>";
+  
+      foreach ($hotel as $key => $value) {
+        echo "<td>" . $value . "</td>";
+      };
+  
+      echo "</tr>";
+      }
+    };
+
+
+  };
 
 
   echo "</tbody></table>";
-
   ?>
+</form>
 
   </body>
 </html>
